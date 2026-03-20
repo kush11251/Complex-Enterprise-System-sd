886929c7fb3b872d233ba79e74f226fd8ae69d27
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-20 10:36:16 +0530

    feat: integrate services/user_service.py

---
+++ services/user_service.py
@@ -0,0 +1,7 @@
+# services/user_service.py
+from models import User
+from repositories import user_repository
+
+class UserService:
+    def get_users(self):
+        return user_repository.get_users()
